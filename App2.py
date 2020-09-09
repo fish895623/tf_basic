6d1dce60d99dbae688e06a835ed84df9e73b2c59
bsh <dan990429@gmail.com>
2020-09-10 00:45:54 +0900

    행곱 계산 tf

--- App2.py
+++ App2.py
@@ -1,3 +1,4 @@
+# %% [markdown]
 import pld
 import tensorflow as tf
 
@@ -7,3 +8,28 @@ print(test)
 
 sess = tf.compat.v1.Session()
 print(sess.run(test))
+
+X_1 = tf.compat.v1.placeholder(tf.float32, [2, 3])
+X_2 = tf.compat.v1.placeholder(tf.float32, [None, 3])
+print(X_1)
+print(X_2)
+# %% [markdown]
+# 텐서플로우를 이용하여 행곱을 연산하는 방법
+X_3 = tf.placeholder(tf.float32, [2, 4])
+x3_data = [[1, 2, 3, 4],
+           [4, 3, 2, 1]]
+
+W_1 = tf.placeholder(tf.float32, [4, 2])
+w1_data = [[0, 1],
+           [1, 2],
+           [2, 3],
+           [3, 4]]
+
+exp = tf.matmul(X_3, W_1)
+session = tf.Session()
+session.run(tf.global_variables_initializer())
+
+a = session.run(exp, feed_dict={X_3: x3_data, W_1: w1_data})
+session.close()
+
+a
